96902340a4815ac06962f7cebb0d52086eb4bc6b
Tomas Montilla <montilla.tom1@gmail.com>
2024-10-12 17:20:58 -0300

    feat: add soft deletion and creation and update timestamp to user entity

--- backend/src/main/java/com/example/tfi_ar/model/User.java
+++ backend/src/main/java/com/example/tfi_ar/model/User.java
@@ -5,9 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.Where;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.Instant;
 import java.util.Collection;
 
 @Data
@@ -16,14 +21,24 @@ import java.util.Collection;
 @AllArgsConstructor
 @Entity
 @Table(name = "user_")
+@SQLDelete(sql = "UPDATE user_ SET deleted = true WHERE id = ?")
+@Where(clause = "deleted = false")
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+    @Column(unique = true)
     private String email;
     private String password;
     @Enumerated(EnumType.STRING)
     private Role role;
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+    private boolean deleted;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
