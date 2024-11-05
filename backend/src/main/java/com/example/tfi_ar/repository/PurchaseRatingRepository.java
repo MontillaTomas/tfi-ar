a6fd4a98b39802486a2d3844c0b2fc2942b703e7
Tomas Montilla <montilla.tom1@gmail.com>
2024-11-05 12:11:12 -0300

    feat: add PurchaseRatingRepository

---
+++ backend/src/main/java/com/example/tfi_ar/repository/PurchaseRatingRepository.java
@@ -0,0 +1,7 @@
+package com.example.tfi_ar.repository;
+
+import com.example.tfi_ar.model.PurchaseRating;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PurchaseRatingRepository extends JpaRepository<PurchaseRating, Integer> {
+}
